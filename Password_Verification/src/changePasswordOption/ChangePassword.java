9bda38f4a1a08f8a5b99cd64b10ede49c7e184ef
vg262 <vg262@demo1.sr.kovair.com>
2018-01-25 04:37:49 -0800

    77

--- Password_Verification/src/changePasswordOption/ChangePassword.java
+++ Password_Verification/src/changePasswordOption/ChangePassword.java
@@ -3,7 +3,7 @@ package changePasswordOption;
 public class ChangePassword {
 
 	public static void main(String[] args) {
-		System.out.print("Change Password ");
+		System.out.print("Change Password 123 ");
 		// TODO Auto-generated method stub
 
 	}
